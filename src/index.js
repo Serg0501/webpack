7021b32858979bd672c46289da11af35ff696735
Serg0501 <gavrilenko87sd@gmail.com>
2022-11-05 12:14:52 +0200

    style+image

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
+import './style.scss';
+import Icon from './png-icon.jpeg';
 
  function component() {
    const element = document.createElement('div');
-   element.innerHTML = _.join(['Hello', 'webpack!!!!!'], ' ');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.classList.add('red');
+
+   // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
    return element;
  }
 
