51126e148a1f895584a30cc19753cb05a798da26
Serg0501 <gavrilenko87sd@gmail.com>
2022-11-05 12:57:54 +0200

    not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
     },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: '',
+            template: './src/index.html',
+            meta: {
+                viewport: "width=device-width, initial-scale=1.0"
+            }
+        }),
+    ],
     module: {
         rules: [
-          {
+          /*{
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
-          },
+          },*/
           {
             test: /\.s[ac]ss$/i,
             use: [
