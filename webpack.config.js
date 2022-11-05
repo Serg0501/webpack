442492540b41487b380a65d443ceed3c5d52fb4e
Serg0501 <gavrilenko87sd@gmail.com>
2022-11-05 13:25:08 +0200

    fix hz how + avtobild + clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build'),
     },
     plugins: [
-        new HTMLWebpackPlugin({
-            title: '',
-            template: './src/index.html',
-            meta: {
-                viewport: "width=device-width, initial-scale=1.0"
-            }
-        }),
-    ],
+      new HTMLWebpackPlugin({
+          template: './src/index.html',
+      }),
+      new CleanWebpackPlugin(),
+  ],
     module: {
         rules: [
           /*{
